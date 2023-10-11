7e8f452a05e4a85ebd7e34d1ac509000e34961ea
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-11 12:02:56 +0100

    added: main.urls

--- main/views.py
+++ main/views.py
@@ -4,3 +4,10 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h4>Test</h4>")
+
+
+def about(request):
+    return HttpResponse(
+        """<h2>This is an a new version of application for our solid state electrochemistry laboratory.</h2>
+        <h4>Here I take into account last half of year experience and will extend
+        a applicatin functional by addition of new equipment.</h4>""")
