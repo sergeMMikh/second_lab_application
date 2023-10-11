b29eaed3c7d5671d36eefe2f8dc3c09a8b86176c
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-11 17:08:09 +0100

    added: CSRF_TRUSTED_ORIGINS to settings.py

--- electrochem_lab/settings.py
+++ electrochem_lab/settings.py
@@ -127,3 +127,5 @@ STATICFILES_STORAGE = "whitenoise.storage.StaticFilesStorage"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+CSRF_TRUSTED_ORIGINS=['https://*.YOUR_DOMAIN.COM']
