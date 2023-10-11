b94c8df4f4115135ca63d9718521839e0692f05c
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-11 09:06:52 +0100

    added: static files

--- electrochem_lab/settings.py
+++ electrochem_lab/settings.py
@@ -122,7 +122,8 @@ USE_TZ = True
 
 STATIC_URL = "static/"
 # STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
