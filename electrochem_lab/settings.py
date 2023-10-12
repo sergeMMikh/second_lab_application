7340a5a12a63d2f536574b5056a0927de9881054
sergeMMikh <100364363+sergeMMikh@users.noreply.github.com>
2023-10-12 10:45:37 +0100

    change: load_dotenv() to load_dotenv(os.path.join(BASE_DIR, ".env"))

--- electrochem_lab/settings.py
+++ electrochem_lab/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 from dotenv import load_dotenv
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,10 +82,14 @@ WSGI_APPLICATION = "electrochem_lab.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        # "ENGINE": "django.db.backends.sqlite3",
+        # "NAME": BASE_DIR / "db.sqlite3",
+        dj_database_url.config(default=DATABASE_URL,
+                               conn_max_age=1800),
     }
 }
 
